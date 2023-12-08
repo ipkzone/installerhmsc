f390e5c3e25d6c1b18a6a4a645f7d00c2e95733b
Ferry Kirdan Agustin <33730041+ipkzone@users.noreply.github.com>
2023-12-08 17:35:33 +0000

    Update installer.php

--- Android/installer.php
+++ Android/installer.php
@@ -1,73 +1,64 @@
 <?php
+error_reporting(0);
+//color
+$Yellow = "\e[33m";
+$Green = "\e[92m";
+$White = "\e[0m";
+$blue = "\e[33m";
 
-namespace ipkzoneLib;
 
-class HmscEnc
-{
-    public static function isHmsc_armv7_php8()
-    {
-        $hmsc = shell_exec('curl https://raw.githubusercontent.com/EddieKidiw/HMSC/main/Linux/Termux/armv7/php8.1/nts/hmsc.so --output hmsc.so');
-        $cp_hmsc = shell_exec('cp hmsc.so /data/data/com.termux/files/usr/lib/php/hmsc.so');
-        $getIni = shell_exec('curl https://raw.githubusercontent.com/ipkzone/axisnet/main/php.ini --output php.ini');
-        $cpini = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/');
-        $cpIni = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/php.ini');
+//choose tools
+echo "\n{$Green}
+    ██╗  ██╗███╗   ███╗███████╗ ██████╗
+    ██║  ██║████╗ ████║██╔════╝██╔════╝
+    ███████║██╔████╔██║███████╗██║     
+    ██╔══██║██║╚██╔╝██║╚════██║██║     
+    ██║  ██║██║ ╚═╝ ██║███████║╚██████╗
+    ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝ ╚═════╝
+    {$White}Installer Libs HMSC Termux linux
+    Author: Iddant ID
+\n";
+echo "
+    ╔═[{$Yellow}Menu{$White}]════════════════════════════
+    ║  {$Yellow}◉{$White} 01. {$Yellow}aarch64 php8.2 NTS{$White}
+    ║  {$Yellow}◉{$White} 02. {$Yellow}arm php8.2 NTS{$White}
+    ║  {$Yellow}◉{$White} 03. {$Yellow}x86_64 php8.2 NTS{$White} ( Emulator )
+    ╚═══════════════════════════════════
+       \n";
 
-        echo "\e[34mINFO\e[0m: Installed lib hmsc.so armv7 PHP 8.1\n";
-    }
+echo " {$Green}➤{$White} Choose: ";
+$pil = trim(fgets(STDIN));
+if ($pil == "1") {
+    $hmsc = shell_exec('curl https://raw.githubusercontent.com/EddieKidiw/HMSC/main/Linux/Termux/aarch64/php8.2/nts/hmsc.so --output hmsc.so');
+    $cp_hmsc = shell_exec('cp hmsc.so /data/data/com.termux/files/usr/lib/php/hmsc.so');
+    $getIni = shell_exec('curl https://raw.githubusercontent.com/ipkzone/axisnet/main/php.ini --output php.ini');
+    $cpini = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/php');
 
-    public static function isHmsc_aarch64_php8()
-    {
-        $hmsc = shell_exec('curl https://raw.githubusercontent.com/EddieKidiw/HMSC/main/Linux/Termux/aarch64/php8.1/nts/hmsc.so --output hmsc.so');
-        $cp_hmsc = shell_exec('cp hmsc.so /data/data/com.termux/files/usr/lib/php/hmsc.so');
-        $getIni = shell_exec('curl https://raw.githubusercontent.com/ipkzone/axisnet/main/php.ini --output php.ini');
-        $cpini = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/');
-        $cpIni = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/php.ini');
+    echo " {$Green}➤{$White} Installed aarch64 php8.2 NTS\n";
+} else if ($pil == "2") {
 
-        echo "\e[34mINFO\e[0m: Installed lib hmsc.so aarch64 PHP 8.1\n";
-    }
-    public static function isHmsc_armv7()
-    {
-        $hmsc = shell_exec('curl https://raw.githubusercontent.com/EddieKidiw/HMSC/main/Linux/Termux/armv7/php7.4/nts/hmsc.so --output hmsc.so');
-        $cp_hmsc = shell_exec('cp hmsc.so /data/data/com.termux/files/usr/lib/php/hmsc.so');
-        $getIni = shell_exec('curl https://raw.githubusercontent.com/ipkzone/axisnet/main/php.ini --output php.ini');
-        $cpini = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/');
-        $cpIni = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/php.ini');
+    $hmsc = shell_exec('curl https://raw.githubusercontent.com/EddieKidiw/HMSC/main/Linux/Termux/arm/php8.2/nts/hmsc.so --output hmsc.so');
+    $cp_hmsc = shell_exec('cp hmsc.so /data/data/com.termux/files/usr/lib/php/hmsc.so');
+    $getIni = shell_exec('curl https://raw.githubusercontent.com/ipkzone/axisnet/main/php.ini --output php.ini');
+    $cpini = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/php');
 
-        echo "\e[34mINFO\e[0m: Installed lib hmsc.so armv7 PHP 7\n";
-    }
+    echo " {$Green}➤{$White} Installed arm php8.2 NTS\n";
+} else if ($pil == "3") {
+    $hmsc = shell_exec('curl https://raw.githubusercontent.com/EddieKidiw/HMSC/main/Linux/Termux/x86_64/php8.2/nts/hmsc.so --output hmsc.so');
+    $cp_hmsc = shell_exec('cp hmsc.so /data/data/com.termux/files/usr/lib/php/hmsc.so');
+    $getIni = shell_exec('curl https://raw.githubusercontent.com/ipkzone/axisnet/main/php.ini --output php.ini');
+    $cpini = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/');
 
-    public static function isHmsc_aarch64()
-    {
-        $hmsc = shell_exec('curl https://raw.githubusercontent.com/EddieKidiw/HMSC/main/Linux/Termux/aarch64/php7.4/zts/hmsc.so --output hmsc.so');
-        $cp_hmsc = shell_exec('cp hmsc.so /data/data/com.termux/files/usr/lib/php/hmsc.so');
-        $getIni = shell_exec('curl https://raw.githubusercontent.com/ipkzone/axisnet/main/php.ini --output php.ini');
-        $cpini = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/');
-        $cpIni = shell_exec('cp php.ini /data/data/com.termux/files/usr/lib/php.ini');
+    echo " {$Green}➤{$White} Installed x86_64 php8.2 NTS\n";
+} else if ($pil == "7") {
+    echo " {$Green}➤{$White} Checking version PHP\n";
+    $a = shell_exec('php -v');
+    echo " {$Green}➤{$White} $a\n";
+} else if ($pil == "8") {
 
-        echo "\e[34mINFO\e[0m: Installed lib hmsc.so aarch64 PHP 7\n";
-    }
-}
-
-error_reporting(E_ERROR);
-
-use \ipkzoneLib\HmscEnc;
-
-if ($argv[1] == "--armv7_php7") {
-    HmscEnc::isHmsc_armv7();
-} elseif ($argv[1] == "--aarch64_php7") {
-    HmscEnc::isHmsc_aarch64();
-} elseif ($argv[1] == "--armv7_php8") {
-    HmscEnc::isHmsc_armv7_php8();
-} elseif ($argv[1] == "--aarch64_php8") {
-    HmscEnc::isHmsc_aarch64_php8();
-} elseif (isset($argv[0])) {
-    $Yellow = "\e[33m";
-    $White = "\e[0m";
-    $Green = "\e[92m";
-    echo " \e[34mINFO\e[0m: Installing lib HMSC\n";
-    echo " \e[34mINFO\e[0m: php name_file.php --armv7_php7\n";
-    echo " \e[34mINFO\e[0m: php name_file.php --aarch64_php7\n";
-    echo " \e[34mINFO\e[0m: php name_file.php --armv7_php8\n";
-    echo " \e[34mINFO\e[0m: php name_file.php --aarch64_php8\n";
-    // echo " \e[34mINFO\e[0m: Get licensi for activasi tools contact admin ( {$Yellow}0895375136311{$White} )\n";
+    echo " {$Green}➤{$White} Checking karnel termux\n";
+    $a = shell_exec('uname -a');
+    $b = shell_exec('dpkg --print-architecture');
+    echo " {$Green}➤{$White} $a\n";
+    echo " {$Green}➤{$White} Your karnel: $b\n";
 }
