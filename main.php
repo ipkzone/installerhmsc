b24e9f2c0ad236dbc4d4fa7d9a24b3dc03a1b453
Ferry Kirdan Agustin <33730041+ipkzone@users.noreply.github.com>
2023-12-14 17:53:47 +0000

    Update main.php

--- main.php
+++ main.php
@@ -15,9 +15,10 @@ echo "
 
     ╔═[{$Yellow}Menu{$White}]════════════════════════════
     ║  {$Yellow}◉{$White} 01. {$Yellow}Windows HMSC PHP 7.3{$White}
-    ║  {$Yellow}◉{$White} 02. {$Yellow}Windows HMSC PHP 8.0{$White}
-    ║  {$Yellow}◉{$White} 03. {$Yellow}Check SystemInfo PC{$White}
-    ║  {$Yellow}◉{$White} 03. {$Yellow}Check PHP Version{$White}
+    ║  {$Yellow}◉{$White} 02. {$Yellow}Windows HMSC PHP 7.4{$White}
+    ║  {$Yellow}◉{$White} 03. {$Yellow}Windows HMSC PHP 8.0{$White}
+    ║  {$Yellow}◉{$White} 04. {$Yellow}Check SystemInfo PC{$White}
+    ║  {$Yellow}◉{$White} 05. {$Yellow}Check PHP Version{$White}
     ╚═══════════════════════════════════
        \n";
 
@@ -36,14 +37,32 @@ if ($pil == "1") {
     if ($pil == "1") {
         $hmsc = shell_exec('copy Windows\php7.3\nts\php_hmsc.dll C:\xampp\php\ext');
         $copy = shell_exec('copy phpini\php.ini C:\xampp\php');
-        echo " {$Green}➤{$White} Installed lib php hmsc Windows x86 php7.3 nts\n";
+        echo " {$Green}➤{$White} Installed lib php hmsc Windows php7.3 nts\n";
     } else if ($pil == "2") {
         $hmsc = shell_exec('copy Windows\php7.3\zts\php_hmsc.dll C:\xampp\php\ext');
         $copy = shell_exec('copy phpini\php.ini C:\xampp\php');
-        echo " {$Green}➤{$White} Installed lib php hmsc Windows x86 php7.4 nts\n";
+        echo " {$Green}➤{$White} Installed lib php hmsc Windows php7.3 nts\n";
     }
 } else if ($pil == "2") {
 
+    echo "
+    ╔═[{$Yellow}Menu{$White}]════════════════════════════
+    ║  {$Yellow}◉{$White} 01. {$Yellow}PHP 7.4 NTS{$White}
+    ║  {$Yellow}◉{$White} 02. {$Yellow}PHP 7.4 ZTS{$White}
+    ╚═══════════════════════════════════
+       \n";
+    echo " {$Green}➤{$White} Choose: ";
+    $pil = trim(fgets(STDIN));
+    if ($pil == "1") {
+        $hmsc = shell_exec('copy Windows\php7.4\nts\php_hmsc.dll C:\xampp\php\ext');
+        $copy = shell_exec('copy phpini\php.ini C:\xampp\php');
+        echo " {$Green}➤{$White} Installed lib php hmsc Windows php7.4 zts\n";
+    } else if ($pil == "2") {
+        $hmsc = shell_exec('copy Windows\php7.4\zts\php_hmsc.dll C:\xampp\php\ext');
+        $copy = shell_exec('copy phpini\php.ini C:\xampp\php');
+        echo " {$Green}➤{$White} Installed lib php hmsc Windows php7.4 zts\n";
+    }
+} else if ($pil == "3") {
     echo "
     ╔═[{$Yellow}Menu{$White}]════════════════════════════
     ║  {$Yellow}◉{$White} 01. {$Yellow}PHP 8.0 NTS{$White}
@@ -55,16 +74,16 @@ if ($pil == "1") {
     if ($pil == "1") {
         $hmsc = shell_exec('copy Windows\php8.0\nts\php_hmsc.dll C:\xampp\php\ext');
         $copy = shell_exec('copy phpini\php.ini C:\xampp\php');
-        echo " {$Green}➤{$White} Installed lib php hmsc Windows x86 php7.3 zts\n";
+        echo " {$Green}➤{$White} Installed lib php hmsc Windows php8.0 zts\n";
     } else if ($pil == "2") {
         $hmsc = shell_exec('copy Windows\php8.0\zts\php_hmsc.dll C:\xampp\php\ext');
         $copy = shell_exec('copy phpini\php.ini C:\xampp\php');
-        echo " {$Green}➤{$White} Installed lib php hmsc Windows x86 php7.4 zts\n";
+        echo " {$Green}➤{$White} Installed lib php hmsc Windows php8.0 zts\n";
     }
-} else if ($pil == "5") {
+} else if ($pil == "4") {
     $sysinfo = shell_exec('systeminfo');
     echo "{$Green}" . $sysinfo . "{$White}\n";
-} else if ($pil == "6") {
+} else if ($pil == "5") {
     $phpv = shell_exec('php -v');
     echo "{$Green}" . $phpv . "{$White}\n";
 }
